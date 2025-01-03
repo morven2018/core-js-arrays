5208ffb3cda941addf1564f4631b230f15825eee
morven2018 <you@example.com>
2025-01-03 21:02:47 +0300

    feat: add createChunks

--- src/arrays-tasks.js
+++ src/arrays-tasks.js
@@ -338,8 +338,13 @@ function calculateBalance(arr) {
  *    createChunks(['a', 'b', 'c', 'd', 'e'], 2) => [['a', 'b'], ['c', 'd'], ['e']]
  *    createChunks([10, 20, 30, 40, 50], 1) => [[10], [20], [30], [40], [50]]
  */
-function createChunks(/* arr, chunkSize */) {
-  throw new Error('Not implemented');
+function createChunks(arr, chunkSize) {
+  const len = Math.ceil(arr.length / chunkSize);
+  return Array(len)
+    .fill([])
+    .map((item, index) => {
+      return arr.slice(index * chunkSize, (index + 1) * chunkSize);
+    });
 }
 
 /**
